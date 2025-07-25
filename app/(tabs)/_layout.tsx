a8af709ec620a6ac5f06bab2fe98c0592f55fec6
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-25 11:33:23 -0300

    feat: add assets

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,6 +8,18 @@ const _Layout = () => {
         name="index"
         options={{ headerShown: false, title: "Home" }}
       />
+      <Tabs.Screen
+        name="search"
+        options={{ headerShown: false, title: "Search" }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{ headerShown: false, title: "Saved" }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ headerShown: false, title: "Profile" }}
+      />
     </Tabs>
   );
 };
